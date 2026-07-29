9bdd4c322cbdfacae931b3bf378a43377d24afd1
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-07-29 21:27:51 +0800

    Add files via upload

---
+++ Codechef contest 182/Move_Grid.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int a,b,c,d;
+   cin >> a>> b >> c >> d;
+   int x = a-c;
+   int y = b-d;
+   cout << x << " " << y;
+    return 0;
+}
