c3e71dbea12bbc04d4a8c2e8a02cc03e79bef6f9
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-07-30 13:33:37 +0800

    Add files via upload

---
+++ Codechef contest 183/B_St_Chroma.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t;
+   cin >> t;
+   while(t--)
+   {
+    int n,x;
+    cin >> n >> x;
+    vector< int > v;
+    for( int i=0;i<x;i++)
+    {
+        v.push_back(i);
+    }
+    for(int i=n-1;i>=x;i--)
+    {
+        v.push_back(i);
+    }
+    v.push_back(x);
+
+    for (int i = 0; i < n;i++)
+     {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+    return 0;
+}
