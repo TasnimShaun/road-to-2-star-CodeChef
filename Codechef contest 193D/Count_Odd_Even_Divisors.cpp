2872ab465f737b069f493391ce901f500d895056
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-08-04 21:46:30 +0800

    Add files via upload

---
+++ Codechef contest 193D/Count_Odd_Even_Divisors.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t;
+   cin >> t;
+   while(t--)
+   {
+       int n;
+       cin >> n ; 
+       int even =0, odd =0;
+           for (int i = 1; i <= n; i++)
+            {
+        if (n% i == 0) 
+        {
+            if (i % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+    }
+    cout << odd <<" " << even << endl;
+
+
+   }
+   return 0;
+}
