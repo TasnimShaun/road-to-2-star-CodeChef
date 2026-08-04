cc6298d632e9c6cc124e5501441be622d469813d
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-08-04 21:50:13 +0800

    Add files via upload

---
+++ Codechef contest 194D/Chef_Bakes_Cake_1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int m,n;
+   cin >> m >> n;
+   int a = m*30;
+   int b= n*50;
+   int x = b-a;
+   cout << x << endl;
+
+    return 0;
+}
