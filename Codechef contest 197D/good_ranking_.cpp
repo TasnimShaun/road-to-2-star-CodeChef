985cbbc3db49626831a46d1c25dbf83284fafee9
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-08-06 21:11:58 +0800

    Add files via upload

---
+++ Codechef contest 197D/good_ranking_.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+    int n;
+    cin >> n;
+     cout << "2" << endl;
+     for(int i=1; i<=n;i++)
+      {
+        cout << i <<" ";
+        
+      }
+      cout << endl;
+        for( int j =n ; j>=1;j--){
+        cout << j  << " ";
+      }
+      cout << endl;
+    return 0;
+}
