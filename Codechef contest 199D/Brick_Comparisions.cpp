4d561ee7c4dcacc76386b63323f4037369c5d9de
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-08-08 21:55:19 +0800

    Add files via upload

---
+++ Codechef contest 199D/Brick_Comparisions.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t;
+   cin >> t;
+   while(t--)
+   {
+        int n;
+        cin >> n;
+        vector< int> v(n+1);
+        for( int i=1;i<=n;i++)
+        {
+            cin >> v[i];
+        }
+         int mxidx =1;
+          for (int i = 2; i <= n; i++)
+           {
+        if (v[i] > v[mxidx]) 
+        
+        {
+           mxidx = i; 
+        }
+    }
+     cout << mxidx << endl;
+
+
+   }
+   return 0;
+}
